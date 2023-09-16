4577397aaa646a3847b2dc3055c1a566049be4ca
Albina <alblupynos@gmail.com>
2023-09-16 14:56:28 +0300

    Fix Date.

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -11,7 +11,7 @@ const content = document.getElementById('content')
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
 
 //GET request to the OpenWeatherMap API
 const fetchWeather = async (baseURL, zipCode, apiKey) => {
